91cdfa24736a871b43babd2357611bf7fe7e6f32
ornel77 <t.ornel@yahoo.fr>
2020-03-31 11:00:50 +0200

    page ok. READY

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 /* ------------------------------- GET Element ------------------------------ */
 const navbar = document.querySelector('.navbar')
 const separation = document.querySelector('.separation')
-const openIcon = document.querySelector('.open')
-const closeIcon = document.querySelector('.close')
+// const openIcon = document.getElementById('open-icon')
+const closeIcon = document.getElementById('close-icon')
+
+const hamburger = document.querySelector('.hamburger')
 
 
-if (window.innerWidth > 600) {
-    console.log('dfgdfg');
-    closeIcon.style.display = 'none'
-    openIcon.style.display = 'none'
+// openIcon.addEventListener('click', () => {
+//     navbar.classList.toggle('active')
+//     openIcon.style.display = 'none'
+//     closeIcon.style.display = 'flex'
+//     // separation.textContent = '------------------'
+// })
 
+// closeIcon.addEventListener('click', ()=>{
+//     navbar.classList.remove('active')
+//     openIcon.style.display = 'initial'
+//     closeIcon.style.display = 'none'
+// })
 
-}
-
-
-
-
-openIcon.addEventListener('click', () => {
-    navbar.classList.add('active')
-    openIcon.style.display = 'none'
-    closeIcon.style.display = 'flex'
-    // separation.textContent = '------------------'
-})
-
-closeIcon.addEventListener('click', ()=>{
-    navbar.classList.remove('active')
-    openIcon.style.display = 'flex'
-    closeIcon.style.display = 'none'
+hamburger.addEventListener('click', () => {
+    document.body.classList.toggle('active')
+    // navbar.classList.toggle('active')
 })
